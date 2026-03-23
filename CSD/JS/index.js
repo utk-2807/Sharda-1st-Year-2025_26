671f27c8c2e7140f84a8bf9711ce06bdb6d42e6d
utk-2807 <rajm3587@gmail.com>
2026-03-23 11:53:39 +0530

    js added

---
+++ CSD/JS/index.js
@@ -0,0 +1,69 @@
+// Javascript is a synchronous and single threaded programming language. (it means one linne of code is executed at a time)
+
+// console.log("Hello World"); //print statement
+
+// 2 types of datatypes are there : -
+// 1. Primitive - Integer, String, null, undefined, boolean. They are stored in Stack Memory
+// 2. Non-Primitive - Array, Object, Symbols, Functions. They are stored inside Heap Memory.
+
+// var, let or const
+
+// var a; //declared the variable a
+// a = 10; // assigning a value to the variable a
+// console.log(typeof(a));
+
+// var b = "Utkarsh ";
+// var c = "Upadhyay";
+// var x = b + c; // Concatenation of string
+// console.log(x);
+
+// var a = [20, "utkarsh", null, true, undefined]
+// a[2] = 'upadhyay';
+// console.log(a);
+
+// Objects
+// const user = {
+//     fname : "Utkarsh",
+//     lname : "Upadhyay",
+//     email : 'utk2807@gmail.com',
+//     state : "Bihar"
+// }
+// user.state = "Delhi";
+// console.log(user['email']);
+
+// var, let and const difference
+
+// var a; //declarijng the variable
+// a = 10; //assigning a value to it
+// var a; //redeclaration is possible
+// a = 50; //reassignment is possible
+// console.log(a);
+
+// let a; //declare
+// a = 20; // assigned a value to it
+// // let a; // redeclaration is not possible in let
+// a = 50; //reassignment is possibe
+// console.log(a);
+
+// const a = 20;
+// a = 60; 
+// console.log(a); // neither redeclare nor reassign is possible
+
+//Functions
+
+// 1st Way
+// function user(){
+//     //code
+// }
+
+// 2nd Way
+// const user = function(){
+//     //code
+// }
+
+// 3rd Way
+// Arrow Function
+// const user = () => {
+//     //code
+// }
+
